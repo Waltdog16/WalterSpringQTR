82dc6c81d586cd19bf0a40a4566b8f48e72d8934
Walter Survine <walter.survine@hgs.hiddengeniusproject.org>
2026-05-13 18:05:57 -0700

    Created new xcode project
    Wlaters work from AI and Game dev are either corrupted or not on his mac,

---
+++ WalterSpringProject/WalterSpringProject/WalterSpringProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalterSpringProjectApp.swift
+//  WalterSpringProject
+//
+//  Created by Walter Survine on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WalterSpringProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
